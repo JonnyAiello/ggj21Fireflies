7c52bcfb5db755028a763994c2f98cd38d1443fc
Jonny Aiello <jonny.aiello@gmail.com>
2020-05-08 20:36:42 -0400

    Updates controls and joystic bindings
    - adds support for using horizontal analagoue input in PCinput
    - adds specific input references for jump/dash, binds them to keyboard
    and controller inputs

--- Assets/Code/Player/PCInput.cs
+++ Assets/Code/Player/PCInput.cs
@@ -7,7 +7,6 @@ public class PCInput : MonoBehaviour{
 	// Variables
     [SerializeField] private bool leftButton; 
     [SerializeField] private bool rightButton; 
-
 	[SerializeField] private bool jumpButton; 
     [SerializeField] private bool dashButton; 
 
@@ -18,21 +17,38 @@ public class PCInput : MonoBehaviour{
     public bool DashButton { get{return dashButton;} }
 
 
-    // Update 
+    // [[ ----- INPUT UPDATE ----- ]]
     public void InputUpdate(){    	
         // only engage here, disengage bool in fixed update after PlayerMove
         // has had a chance to process the button push (avoid dropped inputs)
-        if( !leftButton && Input.GetButton("LeftButton") ){ leftButton = true; }
-        if( !rightButton && Input.GetButton("RightButton") ){ rightButton = true; }
+        if( (!leftButton && Input.GetButton("LeftButton"))
+            || Input.GetAxis("Horizontal") < -0.1f ){ 
+            
+            leftButton = true; 
+        }
+        if( (!rightButton && Input.GetButton("RightButton"))
+            || Input.GetAxis("Horizontal") > 0.1f){ 
+
+            rightButton = true; 
+        }
         if( !jumpButton && Input.GetButton("Jump") ){ jumpButton = true; }
-        if( !dashButton && Input.GetButton("Fire1") ){ dashButton = true; }
+        if( !dashButton && Input.GetButton("Dash") ){ dashButton = true; }
     }
 
+    // [[ ----- RESET INPUTS ----- ]]
     public void ResetInputs(){
-        if( leftButton && !Input.GetButton("LeftButton") ){ leftButton = false; }
-        if( rightButton && !Input.GetButton("RightButton") ){ rightButton = false; }
+        if( (leftButton && !Input.GetButton("LeftButton")) 
+            || Input.GetAxis("Horizontal") > -0.1f){
+            
+            leftButton = false; 
+        }
+        if( (rightButton && !Input.GetButton("RightButton"))
+            || Input.GetAxis("Horizontal") < 0.1f){ 
+
+            rightButton = false; 
+        }
         if( jumpButton && !Input.GetButton("Jump") ){ jumpButton = false; }
-        if( dashButton && !Input.GetButton("Fire1") ){ dashButton = false; }
+        if( dashButton && !Input.GetButton("Dash") ){ dashButton = false; }
          
     }
 }
