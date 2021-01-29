18c1ee05d2908394942069a340b6e43959f44aac
jonny@tora <jonny.aiello@gmail.com>
2021-01-29 14:39:20 -0500

    Adds popup text prefab
    - popup texts come with a triggerzone but could be called in other
    code to - PlayAnim()

---
+++ Assets/Code/PopupText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PopupText : MonoBehaviour {
+
+	// Variables
+	public bool triggerZone;
+
+	// Reference Variables
+	public GameObject textMesh;
+	public Animation popupAnim;
+	private TextMeshPro tmp;
+
+	// [[ ----- AWAKE ----- ]]
+	private void Awake(){
+		tmp = textMesh.GetComponent<TextMeshPro>();
+		tmp.color = new Color(1,1,1,0);
+	}
+
+	// [[ ----- PLAY ANIM ----- ]]
+	public void PlayAnim(){
+		popupAnim.Play();
+	}
+
+	// [[ ----- ON TRIGGER ENTER 2D ----- ]]
+	private void OnTriggerEnter2D( Collider2D _other ){
+		if( triggerZone && _other.tag == "Player" ){
+			PlayAnim();
+		}
+	}
+}
