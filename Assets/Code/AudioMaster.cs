f1474cfa1944d23d19d8a44b8a82ecdeab034b60
jonny@tora <jonny.aiello@gmail.com>
2021-01-30 16:25:02 -0500

    Adds divided music files + AudioMaster

---
+++ Assets/Code/AudioMaster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMaster : MonoBehaviour{
+
+	// Variables
+	public static AudioMaster active;
+
+	// Reference Variables
+	public AudioSource music;
+
+	// [[ ----- AWAKE ----- ]]
+	private void Awake(){
+		if( active == null ){ active = this; }
+		else{ Destroy(this); }
+	}
+
+	// [[ ----- START ----- ]]
+	private void Start(){
+		music.Play();
+	}
+
+}
